7bd07c16c39ce56522cb04409d14c45e3ddf2b7b
mediocrefred <kalinowski_c@lynchburg.edu>
2019-04-24 12:43:40 -0400

    Migrated from private and removed client  tokens

---
+++ gary.js
@@ -0,0 +1,91 @@
+const {Client, RichEmbed} = require('discord.js')
+const client = new Client()
+
+client.on('ready', () => {
+    // List servers the bot is connected to
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login(/*client token goes here*/)
+
+
+// This is where Gary will post a meme everyday
+var schedule = require('node-schedule');
+var meme = require('./post_meme.js');
+var post_meme_job = schedule.scheduleJob('25 16 * * *', function(){
+    const embed = new RichEmbed()    
+    meme.post_meme(client, embed);
+    });
+
+
+// This is where Gary will handle all the messages coming in
+
+client.on('message', (receivedMessage) => {
+    //~ // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage.content.includes(client.user.toString())) {
+        console.log(receivedMessage.author.toString(),receivedMessage.content)
+        // Send acknowledgement message
+        receivedMessage.channel.send("Thanks, bro!")
+    }
+    //Handle different Commands
+    if (receivedMessage.content.startsWith("!")) {
+        try {
+            processCommand(receivedMessage);
+        } catch(e) {
+            console.log(e)
+        }
+    }
+})
+
+
+// This is where Gary will process commands received via messages
+
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
+
+   var argument_list = new Array();
+    //it will look at a response like !2d8 and figure out that it wants 2 8 sided dice rolled
+    if (argument_list = primaryCommand.match(/(\d*)(d)(\d*)/)) {
+        console.log("Argument List" + argument_list)
+        if (argument_list.length == 4 && argument_list[2] == 'd') {
+        if (argument_list[1] != '') {
+                argument_list[1] = parseInt(argument_list[1]);
+        }
+        argument_list[3] = parseInt(argument_list[3]);
+        rollDiceCommand(argument_list, receivedMessage);
+        }
+    }
+    else {
+        receivedMessage.channel.send("I don't understand the command. Try `!d20` or '!2d8'.")
+    }
+}
+
+function rollDiceCommand(argument_list, receivedMessage) {
+    try {
+        if (typeof(argument_list[1]) == "number" && argument_list[1] != '' && typeof(argument_list[3]) == "number") {
+        var rolls = new Array();
+        for (i = 0; i < argument_list[1]; i++) {
+            rolls.push(Math.floor((Math.random() * argument_list[3]) + 1));
+        }
+        receivedMessage.channel.send("You rolled " + rolls)
+        }
+    else if (typeof(argument_list[1]) != "number" && typeof(argument_list[3]) == "number") {
+            receivedMessage.channel.send("You rolled a " + Math.floor((Math.random() * argument_list[3]) + 1));
+        }
+    }
+    catch(err) {
+    console.log(err.stack);
+    receivedMessage.channel.send("Something is wrong here. Might be me, might be you, I dunno...")
+    }
+}
---
+++ post_meme.js
@@ -0,0 +1,60 @@
+
+module.exports = {
+    post_meme : function(client, embed){
+        var fs = require('fs');
+        try {
+            // Get the post id (for posting full url
+            try {
+                var post_url = fs.readFileSync('post_url.txt', 'utf8');
+                console.log(post_url);
+            } catch(e) {
+                console.log('Error getting post URL:', e.stack);
+            }
+            //get the picture url
+            try {
+                var post_image_url = fs.readFileSync('post_image_url.txt', 'utf8');
+                console.log(post_image_url);
+            } catch(e) {
+                console.log('Error getting post image URL:', e.stack);
+            }
+            // get the username of the OP
+            try {
+                var post_username = fs.readFileSync('post_username.txt', 'utf8');
+                console.log(post_username);
+            } catch(e) {
+                console.log('Error getting post username:', e.stack);
+            }
+            //get the title of the post
+            try {
+                var post_title = fs.readFileSync('post_title.txt', 'utf8');
+                console.log(post_title);
+            } catch(e) {
+                console.log('Error getting post title:', e.stack);
+            }
+            //Post the meme
+            var channel_list = ['496100600644239380', '534054412222464002']; // Replace with known channel ID dndmemes: 496100600644239380 test channel: 534054412222464002
+            for (let channel of channel_list) {
+                var generalChannel = client.channels.get(channel) 
+                try {
+                    // Set the title of the field
+                    embed.setTitle('Top post on r/DnDMemes from the past 24 hours')
+                    // Set the URL for the embed
+                    embed.setURL(post_url)
+                    // Set the color of the embed
+                    embed.setColor(0xFF0000)
+                    // Set the main content of the embed
+                    embed.setDescription(post_title);
+                    // Set the image of the embed
+                    embed.setImage(post_image_url)
+                    // Send the embed to the same channel as the message
+                    generalChannel.send(embed);
+                    //generalChannel.send("**Top post on r/DnDMemes from the past 24 hours\nAuthor: **" + post_username + "**\nTitle: **" + post_title + "\n" + post_image_url + '\n' + '<' + post_url + '>')
+                } catch(e) {
+                    console.log('Error posting the meme:', e.stack);
+                }
+            }
+        } catch(e) {
+            console.log('Error posting the meme:', e.stack);
+        }
+    }
+};
