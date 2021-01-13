39d2ad96205aa2005135046acec71b3ac06ff9fe
Casey Kalinowski <32444519+MediocreFred@users.noreply.github.com>
2021-01-13 12:11:44 -0500

    V2.0.0 (#29)
    * Made a config file
    
    * new line at the end of the config file
    
    * Command updates (#24)
    
    * Made some command changes
    
    * relocated the commands
    
    * updates
    
    * Complete command overhaul (#23)
    
    * added new file
    
    * eslint changes
    
    * new gary and old gary
    
    * beep
    
    * kick
    
    * meme command work
    
    * removing some files
    
    * Making commands even better
    
    * cooldown
    
    * Package updates and gitignore update
    
    * Made it to the embeds here https://discordjs.guide
    
    * Rolling command updated to new format
    
    * Finished setting up the command structure...for now.
    
    * Finished setting up the command structure...for now.
    
    Co-authored-by: Casey Kalinowski <kalinowski_c@alum.lynchburg.edu>
    Co-authored-by: Casey Kalinowski <MediocreFred@users.noreply.github.com>
    
    * Enhanced the logging a little
    
    * Made changes
    
    Co-authored-by: Casey Kalinowski <MediocreFred@users.noreply.github.com>
    Co-authored-by: Casey Kalinowski <kalinowski_c@alum.lynchburg.edu>
    
    * Moved two commands into new directory
    
    * modified backfill.py
    
    * Modified the storing of memes
    
    * Modified the message back
    
    * update meme database
    
    * Refactored code to match eslint rules
    
    * New memes added
    
    * Update package-lock.json
    
    * Reformat to match eslint rules
    
    * New command (#28)
    
    * started work on stock.js
    
    * Finished initial work on stock command
    
    * # Conflicts:
    #	config.json
    #	gary.js
    #	gary_channel_list.js
    #	package-lock.json
    #	post_meme.js
    
    * Updated package-lock.json
    
    Co-authored-by: Casey Kalinowski <MediocreFred@users.noreply.github.com>
    Co-authored-by: Casey Kalinowski <kalinowski_c@alum.lynchburg.edu>

---
+++ privateCommands/post_meme.js
@@ -0,0 +1,62 @@
+const config = require('../config.json')
+require('log-timestamp')
+
+module.exports = {
+  name: 'post top meme',
+  description: 'posts',
+  execute (client, embed, subreddit) {
+    const request = require('request')
+    const fs = require('fs')
+    const path = require('path')
+
+    request({
+      headers: {
+        'User-Agent': 'personal:Gary:v1.1Davout (by /u/broseidon16)'
+      },
+      uri: 'https://www.reddit.com/r/' + subreddit + '/top/.json?sort=top&t=day&limit=10',
+      method: 'GET'
+    }, function (err, res, body) {
+      const data = JSON.parse(body)
+      let index = 0
+      // this loops through to find the first non-text post
+      while (data.data.children[index].data.is_self !== false && data.data.children[index].data.over_18 !== false && index < 10) {
+        index = index + 1
+      }
+      // get the post url
+      const postUrl = 'https://www.reddit.com/r/' + subreddit + '/comments/' + data.data.children[index].data.id.replace(/['"]+/g, '')
+      // get the image url
+      const postImageUrl = data.data.children[index].data.url.replace(/['"]+/g, '')
+      // get the poster's username
+      // const post_username = data['data']['children'][index]['data']['author'].replace(/['"]+/g, '');
+      // get the post's title
+      const postTitle = data.data.children[index].data.title.replace(/['"]+/g, '')
+
+      // Save the meme
+      fs.appendFileSync(path.resolve(__dirname, '../memeData', subreddit + '.txt'), '\n' + postImageUrl)
+
+      // Post the meme
+      // Place known channel ID in list below
+      const channelList = config.legacy_channels
+      for (const channel of channelList) {
+        const generalChannel = client.channels.cache.get(channel)
+        try {
+          // Set the title of the field
+          embed.setTitle('Top post on r/' + subreddit + ' from the past 24 hours')
+          // Set the URL for the embed
+          embed.setURL(postUrl)
+          // Set the color of the embed
+          embed.setColor(0xFF0000)
+          // Set the main content of the embed
+          embed.setDescription(postTitle)
+          // Set the image of the embed
+          embed.setImage(postImageUrl)
+          // Send the embed to the same channel as the message
+          generalChannel.send(embed)
+          console.log('Posted a meme from r/' + subreddit)
+        } catch (e) {
+          console.log('Error posting the meme:', e.stack)
+        }
+      }
+    })
+  }
+}
