a30cdb66bcf1ba326109a7cb43f1f3ae31f93414
ckalinowski <casey.kalinowski@tivo.com>
2021-07-01 14:19:53 -0400

    Added simple event handling

--- gary.js
+++ gary.js
@@ -3,6 +3,19 @@ const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    }
+    else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -15,11 +28,6 @@ for (const file of commandFiles) {
 }
 
 const cooldowns = new Discord.Collection();
-
-client.on('ready', () => {
-    // List servers the bot is connected to
-    console.log(`Logged in as ${client.user.tag}!`);
-});
 
 // This is where Gary will handle all the messages coming in
 client.on('message', (message) => {
