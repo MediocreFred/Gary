304f003b5798b7e90024d97dd5b09bcbfd9e5981
Casey Kalinowski <32444519+MediocreFred@users.noreply.github.com>
2021-07-01 17:25:08 -0400

    Dsicord.js v13 upgrade preparations (#37)
    * Added TODO
    
    * Formatting changes/ESLint upgrade
    
    * Remove erlpack
    
    * Added simple event handling
    
    * ToDo updates
    
    Co-authored-by: ckalinowski <casey.kalinowski@tivo.com>

--- commands/meme.js
+++ commands/meme.js
@@ -1,31 +1,31 @@
-const fs = require('fs')
-const path = require('path')
-const Discord = require('discord.js')
-require('log-timestamp')
+const fs = require('fs');
+const path = require('path');
+const Discord = require('discord.js');
+require('log-timestamp');
 
 module.exports = {
-  name: 'meme',
-  description: 'Posts a random meme',
-  cooldown: 10,
-  execute (message, args) {
+    name: 'meme',
+    description: 'Posts a random meme',
+    cooldown: 10,
+    execute(message, args) {
     // files can be found in the directory 'memeData'
-    const files = ['DnDMemes.txt', 'Memes.txt', 'TrippinThroughTime.txt', 'DankMemes.txt', 'LotRMemes.txt', 'PrequelMemes.txt']
-    let fileSelection = files[Math.floor(Math.random() * files.length)]
-    let memes = fs.readFileSync(path.resolve(__dirname, '../memeData', fileSelection)).toString().split('\n')
-    // message.channel.send('Meme!');
-    let meme = ''
-    while (meme === '') {
-      meme = memes[Math.floor(Math.random() * memes.length)].replace(/[\n\r]/g, '')
-    }
+        const files = ['DnDMemes.txt', 'Memes.txt', 'TrippinThroughTime.txt', 'DankMemes.txt', 'LotRMemes.txt', 'PrequelMemes.txt'];
+        const fileSelection = files[Math.floor(Math.random() * files.length)];
+        const memes = fs.readFileSync(path.resolve(__dirname, '../memeData', fileSelection)).toString().split('\n');
+        // message.channel.send('Meme!');
+        let meme = '';
+        while (meme === '') {
+            meme = memes[Math.floor(Math.random() * memes.length)].replace(/[\n\r]/g, '');
+        }
 
-    // format the post
-    const messageEmbed = new Discord.MessageEmbed()
-      .setTitle('Random meme from ' + 'r/' + fileSelection.slice(0, -4))
-      .setColor(0xFF0000)
-      .setImage(meme)
+        // format the post
+        const messageEmbed = new Discord.MessageEmbed()
+            .setTitle('Random meme from ' + 'r/' + fileSelection.slice(0, -4))
+            .setColor(0xFF0000)
+            .setImage(meme);
 
-    message.channel.send(messageEmbed).then(() => {
-      console.log('Posted a random meme, ' + meme + ', from ' + fileSelection)
-    })
-  }
-}
+        message.channel.send(messageEmbed).then(() => {
+            console.log('Posted a random meme, ' + meme + ', from ' + fileSelection);
+        });
+    },
+};
