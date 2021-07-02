9db926c3084c843fcc9d85938c25289b892a0286
Casey Kalinowski <32444519+MediocreFred@users.noreply.github.com>
2021-07-02 14:09:59 -0400

    Fix gary response to tag (#38)
    * Fixed default reply to tags
    
    * typo
    
    Co-authored-by: ckalinowski <casey.kalinowski@tivo.com>

--- events/message.js
+++ events/message.js
@@ -1,6 +1,15 @@
+require('log-timestamp');
+
 module.exports = {
     name: 'message',
     execute(message) {
         console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
+        if (message.content.includes(message.client.user['id'])) {
+            console.log(message.author.toString(), message.content);
+            // Send acknowledgement message
+            message.reply('thats my name, don\'t wear it out!');
+            console.log('Replying to message with default reply');
+        }
+        else { console.log(message.content + ' ' + message.client.user);}
     },
 };
