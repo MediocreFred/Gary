7990e6a1e1e2aa1b8d4425e7b3dabf838a5dfda8
Casey Kalinowski <casey.kalinowski@xperi.com>
2026-02-11 11:47:17 -0500

    Fix lint stuff

--- events/message.js
+++ events/message.js
@@ -1,26 +1,26 @@
-require('log-timestamp');
+require("log-timestamp");
 
 module.exports = {
-    name: 'messageCreate',
-    execute(message) {
-        console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
+  name: "messageCreate",
+  execute(message) {
+    console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
 
-        if (message.content.includes(message.client.user.id)) {
-            // make bot appear to be typing for a little before answering
-            if (message.channel?.sendTyping) message.channel.sendTyping();
+    if (message.content.includes(message.client.user.id)) {
+      // make bot appear to be typing for a little before answering
+      if (message.channel?.sendTyping) message.channel.sendTyping();
 
-            console.log(message.author.toString(), message.content);
+      console.log(message.author.toString(), message.content);
 
-            // Send acknowledgement message
-            message.reply('thats my name, don\'t wear it out!');
-            console.log('Replying to message with default reply');
-        }
-        if ((message.content === 'Yikes') || (message.content === 'yikes')) {
-            // make bot appear to be typing for a little before answering
-            if (message.channel?.sendTyping) message.channel.sendTyping();
+      // Send acknowledgement message
+      message.reply("thats my name, don't wear it out!");
+      console.log("Replying to message with default reply");
+    }
+    if (message.content === "Yikes" || message.content === "yikes") {
+      // make bot appear to be typing for a little before answering
+      if (message.channel?.sendTyping) message.channel.sendTyping();
 
-            message.channel.send('Yikes indeed!');
-            console.log('Yikes');
-        }
-    },
+      message.channel.send("Yikes indeed!");
+      console.log("Yikes");
+    }
+  },
 };
